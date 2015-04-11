6a672f3ef1ac80a53753305c8ac2254e64600f99
Tema <unlucky@inbox.ru>
2015-04-11 13:11:29 +0800

    now copies not encoded classes

--- decode.php
+++ decode.php
@@ -1,11 +1,11 @@
 <?php
 
-header('Content-type: text/plain');
+$startTime = microtime(true);
 
+// settings
 $classesDir = 'classes';
 $outputDir = 'classes_decoded';
 $exclude = [
-    '.', '..',
     'CreateDocx.inc',
     'AutoLoader.inc',
     'Phpdocx_config.inc',
@@ -13,27 +13,44 @@ $exclude = [
     'Helpers.inc',
 ];
 
-if (!is_dir($outputDir)) {
-    if (!mkdir($outputDir)) {
-        echo 'Unable to make dir ' . $outputDir . PHP_EOL;
-        die();
-    }
+// checks dirs
+if (!is_dir($classesDir)) {
+    echo 'Directory ' . $classesDir . " doesn't exist";
+    die();
 }
-$files = scandir($classesDir);
+if (!is_dir($outputDir) && !mkdir($outputDir)) {
+    echo 'Unable to make dir ' . $outputDir;
+    die();
+}
+
+// decodes encoded classes
+$files = array_diff(scandir($classesDir), ['.', '..'], $exclude);
 foreach ($files as $file) {
-    if (!in_array($file, $exclude)) {
-        $pathToFile = $classesDir . DIRECTORY_SEPARATOR . $file;
-        $base64 = file_get_contents($pathToFile);
-        $inflated = base64_decode($base64, true);
-        $decoded = gzinflate($inflated);
-        if ($base64 && $inflated && $decoded) {
-            $savePath = $outputDir . DIRECTORY_SEPARATOR . $file;
-            if (!file_put_contents($savePath, '<?php' . $decoded)) {
-                echo 'Unable to save ' . $file . PHP_EOL;
-            }
-        } else {
-            echo 'Error with ' . $file . PHP_EOL;
+    $pathToFile = $classesDir . DIRECTORY_SEPARATOR . $file;
+    $base64 = file_get_contents($pathToFile);
+    $inflated = base64_decode($base64, true);
+    $decoded = gzinflate($inflated);
+    if ($base64 && $inflated && $decoded) {
+        $savePath = $outputDir . DIRECTORY_SEPARATOR . $file;
+        if (!file_put_contents($savePath, '<?php' . $decoded)) {
+            echo 'Unable to save ' . $file . '<br>';
         }
+    } else {
+        echo 'Error with ' . $file . '<br>';
     }
 }
-echo 'Done.' . PHP_EOL;
+
+// copies not encoded classes
+foreach ($exclude as $file) {
+    $pathToFile = $classesDir . DIRECTORY_SEPARATOR . $file;
+    $savePath = $outputDir . DIRECTORY_SEPARATOR . $file;
+    if (file_exists($pathToFile)) {
+        if (!copy($pathToFile, $savePath)) {
+            echo 'Unable to save ' . $file . '<br>';
+        }
+    } else {
+        echo 'File ' . $file . " doesn't exist" . '<br>';
+    }
+}
+
+echo 'Done in ' . round(microtime(true) - $startTime, 3) . ' seconds.';
--- readme.md
+++ readme.md
@@ -2,6 +2,15 @@
 
 [http://www.phpdocx.com/](http://www.phpdocx.com/)
 
-Classes loading function in AutoLoader.inc at line 48
+Don't forget to change classes loading function in AutoLoader.inc at line 48
+to something like this:
+
+    public static function autoloadGenericClasses($className)
+    {
+        $pathPhpdocx = dirname(__FILE__) . '/' . $className . '.inc';
+        if (file_exists($pathPhpdocx)) {
+            require_once $pathPhpdocx;
+        }
+    }
 
 Trial message in CreateDocx.inc from line 3223 to 3277
