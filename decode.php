8113ecc9b3c37ed9c9bf8738bcb78df7ea939450
Tema <kukukov.kuku@gmail.com>
2015-09-09 22:05:26 +0800

    codestyle update

--- decode.php
+++ decode.php
@@ -1,7 +1,5 @@
 <?php
 
-$startTime = microtime(true);
-
 // settings
 $classesDir = 'classes';
 $outputDir = 'classes_decoded';
@@ -13,17 +11,15 @@ $exclude = [
     'Helpers.inc',
 ];
 
-// checks dirs
+// check dirs
 if (!is_dir($classesDir)) {
-    echo 'Directory ' . $classesDir . " doesn't exist";
-    die();
+    throw new Exception("Directory '$classesDir' doesn't exist");
 }
 if (!is_dir($outputDir) && !mkdir($outputDir)) {
-    echo 'Unable to make dir ' . $outputDir;
-    die();
+    throw new Exception("Unable to make directory '$outputDir'");
 }
 
-// decodes encoded classes
+// decode encoded classes
 $files = array_diff(scandir($classesDir), ['.', '..'], $exclude);
 foreach ($files as $file) {
     $pathToFile = $classesDir . DIRECTORY_SEPARATOR . $file;
@@ -33,24 +29,24 @@ foreach ($files as $file) {
     if ($base64 && $inflated && $decoded) {
         $savePath = $outputDir . DIRECTORY_SEPARATOR . $file;
         if (!file_put_contents($savePath, '<?php' . $decoded)) {
-            echo 'Unable to save ' . $file . '<br>';
+            echo "Unable to save $file<br>";
         }
     } else {
-        echo 'Error with ' . $file . '<br>';
+        echo "Error with $file<br>";
     }
 }
 
-// copies not encoded classes
+// copy not encoded classes
 foreach ($exclude as $file) {
     $pathToFile = $classesDir . DIRECTORY_SEPARATOR . $file;
     $savePath = $outputDir . DIRECTORY_SEPARATOR . $file;
     if (file_exists($pathToFile)) {
         if (!copy($pathToFile, $savePath)) {
-            echo 'Unable to save ' . $file . '<br>';
+            echo "Unable to save $file<br>";
         }
     } else {
-        echo 'File ' . $file . " doesn't exist" . '<br>';
+        echo "File $file doesn't exist<br>";
     }
 }
 
-echo 'Done in ' . round(microtime(true) - $startTime, 3) . ' seconds.';
+echo 'The end.';
